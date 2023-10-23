9c0775ed53c1f148c75d3ca724b0c31e35ce8279
Cheesemassacre <144286404+Cheesemassacre@users.noreply.github.com>
2023-10-24 00:57:32 +0200

    Add files via upload
    Add dmenu

---
+++ dmenu-5.2/config.h
@@ -0,0 +1,28 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=10"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#070707" },
+	[SchemeSel] = { "#eeeeee", "#6f1d1b" },
+	[SchemeSelHighlight] = { "#bb9457", "#6f1d1b" },
+	[SchemeNormHighlight] = { "#bb9457", "#222222" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+	/* [SchemeHp]  = { "#bbbbbb", "#000000" } */
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
